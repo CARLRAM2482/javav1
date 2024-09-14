a52a63685602347adfc52522f1519e19494bb28e
CARLRAM2482 <145763670+CARLRAM2482@users.noreply.github.com>
2024-09-14 08:37:39 -0500

    V1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HOLA MUNDO");
+        System.out.println("Bienvenidos(a) a screen match");
+        System.out.println("pelicula: Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (8.2 + 6.0 + 9.0) / 3;
+        System.out.println(media);
+
+        String sinopsis = """
+                matrix es una paradoja 
+                la mejor pelicula del fin del milenio
+                fue lanzada en :
+                """ + fechaDeLanzamiento;
+        System.out.println(sinopsis);
+
+        int clasificacion = (int) (media / 1);
+        System.out.println(clasificacion);
+
+
+
     }
 }
